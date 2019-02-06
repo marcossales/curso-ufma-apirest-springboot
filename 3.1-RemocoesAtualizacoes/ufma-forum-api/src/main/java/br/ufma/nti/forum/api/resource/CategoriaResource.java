7cb032e06b4632743eacc9a27d4cae0eb6340f25
marcossales <marcos.vs15@gmail.com>
2019-02-06 20:23:18 -0300

    3.1-RemocoesAtualizacoes

--- 3.1-RemocoesAtualizacoes/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/resource/CategoriaResource.java
+++ 3.1-RemocoesAtualizacoes/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/resource/CategoriaResource.java
@@ -5,14 +5,17 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ufma.nti.forum.api.event.RecursoCriadoEvent;
 import br.ufma.nti.forum.api.model.Categoria;
 import br.ufma.nti.forum.api.repository.CategoriaRepository;
+import br.ufma.nti.forum.api.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
@@ -34,6 +38,9 @@ public class CategoriaResource {
 	
 	@Autowired
 	ApplicationEventPublisher publisher;
+	
+	@Autowired
+	CategoriaService categoriaService;
 	
 	@GetMapping
 	public List<Categoria> listar(){
@@ -60,6 +67,20 @@ public class CategoriaResource {
 	public void remover(@PathVariable Long codigo) {
 		categoriaRepository.delete(codigo);
 	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo,@Valid @RequestBody Categoria categoria){
+		Categoria categoriaSalva = categoriaService.atualizar(codigo, categoria);
+		return ResponseEntity.ok(categoriaSalva);
+	}
+	
+	
+	@PutMapping("/{codigo}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+		categoriaService.atualizarPropriedadeAtivo( codigo, ativo);
+	}
+	
 
 
 }
--- 3.1-RemocoesAtualizacoes/ufma-forum-api/src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ 3.1-RemocoesAtualizacoes/ufma-forum-api/src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,10 +1,11 @@
 CREATE TABLE categoria (
 	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50) NOT NULL
+	nome VARCHAR(50) NOT NULL,
+	ativo BOOLEAN NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO categoria(nome) values('Java');
-INSERT INTO categoria(nome) values('Android');
-INSERT INTO categoria(nome) values('Ciência da Computação');
-INSERT INTO categoria(nome) values('Literatura');
-INSERT INTO categoria(nome) values('Outros');
+INSERT INTO categoria(nome,ativo) values('Java',TRUE);
+INSERT INTO categoria(nome,ativo) values('Android',TRUE);
+INSERT INTO categoria(nome,ativo) values('Ciência da Computação',TRUE);
+INSERT INTO categoria(nome,ativo) values('Literatura',TRUE);
+INSERT INTO categoria(nome,ativo) values('Outros',TRUE);
