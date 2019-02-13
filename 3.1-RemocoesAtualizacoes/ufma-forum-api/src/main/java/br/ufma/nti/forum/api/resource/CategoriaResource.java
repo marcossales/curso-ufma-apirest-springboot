689ba7d22f535bf379233852b3c48ca7b93a9619
marcossales <marcos.vs15@gmail.com>
2019-02-13 02:21:40 -0200

    3.1-RemocoesAtualizacoes

--- 3.1-RemocoesAtualizacoes/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/resource/CategoriaResource.java
+++ 3.1-RemocoesAtualizacoes/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/resource/CategoriaResource.java
@@ -5,10 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
