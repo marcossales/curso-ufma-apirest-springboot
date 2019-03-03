849ab5599829720f41cb765c7e4bb51fce453707
marcossales <marcos.vs15@gmail.com>
2019-03-02 23:45:38 -0300

    5.5-RefreshTokenEmCookie

---
+++ 5.5-RefreshTokenEmCookie/ufma-forum-api/src/test/java/br/ufma/nti/forum/api/UfmaForumApiApplicationTests.java
@@ -0,0 +1,17 @@
+package br.ufma.nti.forum.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UfmaForumApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
