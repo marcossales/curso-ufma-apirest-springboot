5f11d13d5a622466b40e605e8795f4e7f7dae5c5
marcossales <marcos.vs15@gmail.com>
2019-03-03 14:31:09 -0300

    4.4-RemocoesFaltantes

--- 4.4-RemocoesFaltantes/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/model/Mensagem.java
+++ 4.4-RemocoesFaltantes/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/model/Mensagem.java
@@ -13,7 +13,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="mensagem")
