557fe9a763b13a590760f50cc0b0f40385c964a8
marcossales <marcos.vs15@gmail.com>
2019-03-03 13:56:54 -0300

    5.4-RefreshToken

--- 5.4-RefreshToken/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/config/AuthorizationServerConfig.java
+++ 5.4-RefreshToken/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/config/AuthorizationServerConfig.java
@@ -8,9 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
