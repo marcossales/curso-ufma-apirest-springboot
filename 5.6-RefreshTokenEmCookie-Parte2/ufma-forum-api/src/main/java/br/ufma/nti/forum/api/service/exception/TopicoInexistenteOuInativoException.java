907554820abd48f3d25bbaee817c7afad0d1716c
marcossales <marcos.vs15@gmail.com>
2019-03-03 13:51:48 -0300

    5.6-RefreshTokenEmCookie-Parte2

---
+++ 5.6-RefreshTokenEmCookie-Parte2/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/service/exception/TopicoInexistenteOuInativoException.java
@@ -0,0 +1,9 @@
+package br.ufma.nti.forum.api.service.exception;
+
+public class TopicoInexistenteOuInativoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
