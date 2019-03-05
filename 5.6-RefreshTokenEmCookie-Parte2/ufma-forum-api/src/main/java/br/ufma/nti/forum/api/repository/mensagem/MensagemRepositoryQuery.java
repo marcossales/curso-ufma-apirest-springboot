ae729b3b73df8712875bea70e8884afc1e9855b8
marcossales <marcos.vs15@gmail.com>
2019-03-05 00:58:00 -0300

    5.6-RefreshTokenEmCookie-Parte2

--- 5.6-RefreshTokenEmCookie-Parte2/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/repository/mensagem/MensagemRepositoryQuery.java
+++ 5.6-RefreshTokenEmCookie-Parte2/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/repository/mensagem/MensagemRepositoryQuery.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 
 import br.ufma.nti.forum.api.model.Mensagem;
 import br.ufma.nti.forum.api.repository.filter.MensagemFilter;
+import br.ufma.nti.forum.api.repository.projection.ResumoMensagem;
 
 public interface MensagemRepositoryQuery {
 	
 	public Page<Mensagem> filtrar(MensagemFilter mensagemFilter, Pageable pageable);
+	public Page<ResumoMensagem> resumir(MensagemFilter mensagemFilter, Pageable pageable);
 
 }
