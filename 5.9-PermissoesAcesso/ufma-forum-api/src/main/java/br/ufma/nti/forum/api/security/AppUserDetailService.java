a9e6633b5684485c9a87d86584ddfd2c51a50351
marcossales <marcos.vs15@gmail.com>
2019-03-04 22:25:38 -0300

    5.9-PermissoesAcesso

---
+++ 5.9-PermissoesAcesso/ufma-forum-api/src/main/java/br/ufma/nti/forum/api/security/AppUserDetailService.java
@@ -0,0 +1,40 @@
+package br.ufma.nti.forum.api.security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.ufma.nti.forum.api.model.Usuario;
+import br.ufma.nti.forum.api.repository.UsuarioRepository;
+
+@Service
+public class AppUserDetailService implements UserDetailsService{
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+		Usuario usuario = usuarioOptional.orElseThrow(()-> new UsernameNotFoundException("Credenciais não encontradas"));
+		
+		return new User(email, usuario.getSenha(),getPermissoes(usuario));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao())));
+		return authorities;
+	}
+
+}
